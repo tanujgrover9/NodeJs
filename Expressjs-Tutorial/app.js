5d76087c0db8503a216745a5849c18cd2a432574
tanujgrover9 <tanujgrover07@gmail.com>
2024-08-02 12:03:48 +0530

    First commit

--- Expressjs-Tutorial/app.js
+++ Expressjs-Tutorial/app.js
@@ -1,1 +1,30 @@
-console.log("Express js")
+console.log("Express js");
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    // console.log(req.method)
+    //console.log(req.url)
+    const url = req.url;
+    if(url === '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h1>Home Page</h1>')
+        res.end("<h1>Home Page</h1>")
+        
+    }
+    else if (url==='/about'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h1>About Page</h1>');
+        res.end();
+    }
+    else{
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.write('<h1>Opps! Page not found..</h1>')
+        res.end();
+
+    }
+
+})
+
+server.listen(5000,()=>{
+    console.log('server is running on port! 5000...')
+});
